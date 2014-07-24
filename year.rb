ee6f693d66b5aecf892e84020366f43d8a0d1cc3
Stephen Weigel <stephen@stephenweigel.com>
2014-07-24 10:32:43 -0400

    Second Refactor

--- year.rb
+++ year.rb
@@ -1,4 +1,20 @@
-require_relative 'divisible'
+module Divisible
+    def divisible_by?(number, divisor)
+      number % divisor == 0
+    end
+
+    def divisible_by_four_hundred?(number)
+      divisible_by?(number, 400)
+    end
+
+    def divisible_by_one_hundred?(number)
+      divisible_by?(number, 100)
+    end
+
+    def divisible_by_four?(number)
+      divisible_by?(number, 4)
+    end
+end
 
 class Year
   include Divisible
